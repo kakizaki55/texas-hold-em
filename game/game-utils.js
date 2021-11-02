a53144f4cea474952e55faac3d04db35ba880670
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-11-01 17:11:49 -0700

    wrote and tested function createTurn

--- game/game-utils.js
+++ game/game-utils.js
@@ -3,4 +3,12 @@ export function createHand(array){
     return hand;
 }
 
-export function createFlop(array){}
+export function createFlop(array){
+    const flop = (array.splice(0, 3));
+    return flop;
+}
+
+export function createTurn(array){
+    const turn = (array.splice(0, 1));
+    return turn;
+}
