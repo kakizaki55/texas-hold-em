4a6edf5cdcb47b3afc330d72e77e6d9c8c17db57
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-11-02 14:16:29 -0700

    created shuffle array function

--- game/game.js
+++ game/game.js
@@ -110,3 +110,16 @@ export function tableFlop(deck){
     }
 
 }
+shuffle(fakeHand);
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+    return array;
+    
+}
