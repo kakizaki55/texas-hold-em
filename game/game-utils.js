09d605f131cdfc665a2f88b2f3eca0727b66aea6
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-11-01 17:06:55 -0700

    wrote test for createFlop function

--- game/game-utils.js
+++ game/game-utils.js
@@ -2,3 +2,5 @@ export function createHand(array){
     const hand = (array.splice(0, 2));
     return hand;
 }
+
+export function createFlop(array){}
--- utils.js
+++ utils.js
@@ -16,4 +16,3 @@ export function pushLocal(playerObject){
     localStorage.setItem('PLAYER', stringyPlayerObject);
 }
 
-
