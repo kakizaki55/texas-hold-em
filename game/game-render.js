be2dcda7be99276939b2cfe2b46d685d76721dbe
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-11-03 13:17:27 -0700

    clean game.js

---
+++ game/game-render.js
@@ -0,0 +1,101 @@
+export function playerHand(deck){
+    for (let item of deck){
+        const playerTable = document.getElementById('player-hand');
+        const div = document.createElement('div');
+    
+        const img = document.createElement('img');
+        img.src = `${item.image}`;
+    
+        const suit = document.createElement('p');
+        suit.textContent = item.suit;
+    
+        const number = document.createElement('p');
+        number.textContent = item.number;
+    
+        div.append(img, suit, number);
+        playerTable.append(div);
+    }
+
+}
+
+export function dealerHand(deck){
+    for (let item of deck){
+        const dealerTable = document.getElementById('dealer-hand');
+        const div = document.createElement('div');
+    
+        const img = document.createElement('img');
+        img.src = `${item.image}`;
+    
+        const suit = document.createElement('p');
+        suit.textContent = item.suit;
+    
+        const number = document.createElement('p');
+        number.textContent = item.number;
+    
+        div.append(img, suit, number);
+        dealerTable.append(div);
+    }
+
+}
+
+export function tableFlop(deck){
+    for (let item of deck){
+        const table = document.getElementById('table-hand');
+        const div = document.createElement('div');
+    
+        const img = document.createElement('img');
+        img.classList.add('tableCss');
+        img.src = `${item.image}`;
+    
+        const suit = document.createElement('p');
+        suit.textContent = item.suit;
+    
+        const number = document.createElement('p');
+        number.textContent = item.number;
+    
+        div.append(img, suit, number);
+        table.append(div);
+    }
+}
+
+
+export function tableTurn(deck){
+    for (let item of deck){
+        const table = document.getElementById('table-turn');
+        const div = document.createElement('div');
+    
+        const img = document.createElement('img');
+        img.classList.add('tableCss');
+        img.src = `${item.image}`;
+    
+        const suit = document.createElement('p');
+        suit.textContent = item.suit;
+    
+        const number = document.createElement('p');
+        number.textContent = item.number;
+    
+        div.append(img, suit, number);
+        table.append(div);
+    }
+}
+
+
+export function tableRiver(deck){
+    for (let item of deck){
+        const table = document.getElementById('table-river');
+        const div = document.createElement('div');
+    
+        const img = document.createElement('img');
+        img.classList.add('tableCss');
+        img.src = `${item.image}`;
+    
+        const suit = document.createElement('p');
+        suit.textContent = item.suit;
+    
+        const number = document.createElement('p');
+        number.textContent = item.number;
+    
+        div.append(img, suit, number);
+        table.append(div);
+    }
+}
--- game/game.js
+++ game/game.js
@@ -1,140 +1,31 @@
 //render card function
-
+import { playerHand, dealerHand, tableFlop, tableTurn, tableRiver } from './game-render.js';
 import deck from './card-data.js';
 import { createFlop, createHand, createTurn } from './game-utils.js';
 // import { findById, pullLocal, pushLocal } from '../utils.js';
 
-// dealerHand is Flop + Turn + river
-// const dealerHand =
-
-// player hand is just 2 cards
-// const playerHand = createHand();
 shuffle(deck);
-console.log(deck);
 
 const pHand = createHand(deck);
-console.log(pHand, deck);
 
 const dHand = createHand(deck);
-console.log(dHand, deck);
 
 const tFlop = createFlop(deck);
-console.log(tFlop);
 
 const tHand = createTurn(deck);
-console.log(tHand);
 
 const rHand = createTurn(deck);
-console.log(rHand);
-
-
-
 
 
 playerHand(pHand);
 
-// where player hand goes
-function playerHand(deck){
-    for (let item of deck){
-        const playerTable = document.getElementById('player-hand');
-        const div = document.createElement('div');
-    
-        const img = document.createElement('img');
-        img.src = `${item.image}`;
-    
-        const suit = document.createElement('p');
-        suit.textContent = item.suit;
-    
-        const number = document.createElement('p');
-        number.textContent = item.number;
-    
-        div.append(img, suit, number);
-        playerTable.append(div);
-    }
-
-}
 dealerHand(dHand);
-function dealerHand(deck){
-    for (let item of deck){
-        const dealerTable = document.getElementById('dealer-hand');
-        const div = document.createElement('div');
-    
-        const img = document.createElement('img');
-        img.src = `${item.image}`;
-    
-        const suit = document.createElement('p');
-        suit.textContent = item.suit;
-    
-        const number = document.createElement('p');
-        number.textContent = item.number;
-    
-        div.append(img, suit, number);
-        dealerTable.append(div);
-    }
 
-}
 tableFlop(tFlop);
-function tableFlop(deck){
-    for (let item of deck){
-        const table = document.getElementById('table-hand');
-        const div = document.createElement('div');
-    
-        const img = document.createElement('img');
-        img.classList.add('tableCss');
-        img.src = `${item.image}`;
-    
-        const suit = document.createElement('p');
-        suit.textContent = item.suit;
-    
-        const number = document.createElement('p');
-        number.textContent = item.number;
-    
-        div.append(img, suit, number);
-        table.append(div);
-    }
-}
 
 tableTurn(tHand);
-function tableTurn(deck){
-    for (let item of deck){
-        const table = document.getElementById('table-turn');
-        const div = document.createElement('div');
-    
-        const img = document.createElement('img');
-        img.classList.add('tableCss');
-        img.src = `${item.image}`;
-    
-        const suit = document.createElement('p');
-        suit.textContent = item.suit;
-    
-        const number = document.createElement('p');
-        number.textContent = item.number;
-    
-        div.append(img, suit, number);
-        table.append(div);
-    }
-}
 
 tableRiver(rHand);
-function tableRiver(deck){
-    for (let item of deck){
-        const table = document.getElementById('table-river');
-        const div = document.createElement('div');
-    
-        const img = document.createElement('img');
-        img.classList.add('tableCss');
-        img.src = `${item.image}`;
-    
-        const suit = document.createElement('p');
-        suit.textContent = item.suit;
-    
-        const number = document.createElement('p');
-        number.textContent = item.number;
-    
-        div.append(img, suit, number);
-        table.append(div);
-    }
-}
 
 
 shuffle(deck);
@@ -160,8 +51,6 @@ dealButton.addEventListener('click', (e)=>{
     tableSection.classList.add('unHidden');
     dealerSection.classList.remove('hidden');
     faceDown.classList.add('hidden');
-
-
 });
 
 refreshButton.addEventListener('click', (e)=>{
